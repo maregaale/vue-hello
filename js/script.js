df3fccef21c577c1cdab32f99c2ddb3840a32bee
Alessandro Marega <ale_marega@hotmail.it>
2021-03-24 16:44:41 +0100

    aggiunto v-model a tag input e stampa in p, aggiunto evento al click sul bottone

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@ const app = new Vue (
     el: '#root',
     data: {
       saluto: 'Hello Vue!',
-      srcImg: "http://docmac.it/wp-content/uploads/2016/05/Hello_Hello-Again-e1501025061872.jpg"
-    }
+      srcImg: "http://docmac.it/wp-content/uploads/2016/05/Hello_Hello-Again-e1501025061872.jpg",
+      message: "",
+    },
+    methods: {
+      cambiaImmagine: function () {
+        if (app.srcImg == "http://docmac.it/wp-content/uploads/2016/05/Hello_Hello-Again-e1501025061872.jpg") {
+          this.srcImg = "https://casellaweb.com/wp-content/uploads/2012/09/HelloWorld-680x167.png";
+        } else {
+          this.srcImg = "http://docmac.it/wp-content/uploads/2016/05/Hello_Hello-Again-e1501025061872.jpg";
+        }
+      }
+    },
   }
 );
