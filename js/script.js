4fa07ec79c7ef2ad91f2fab1efd85553a9e908bf
Alessandro Marega <ale_marega@hotmail.it>
2021-03-24 15:36:26 +0100

    aggiunta stampa in h1 con Vue e aggiunta immagine

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue (
+  {
+    el: '#root',
+    data: {
+      saluto: 'Hello Vue!',
+      srcImg: "http://docmac.it/wp-content/uploads/2016/05/Hello_Hello-Again-e1501025061872.jpg"
+    }
+  }
+);
